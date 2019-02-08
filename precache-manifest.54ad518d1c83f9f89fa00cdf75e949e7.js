e6f3a7e80391ec219f1c3a2a7534f6c617d56d75
Maxine <emaxan1997@gmail.com>
2019-02-08 18:23:25 +0300

    Updates

---
+++ precache-manifest.54ad518d1c83f9f89fa00cdf75e949e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "44df1775f36ae7b0e17e",
+    "url": "/static/js/main.44df1775.chunk.js"
+  },
+  {
+    "revision": "e4add4823f015118b25c",
+    "url": "/static/js/1.e4add482.chunk.js"
+  },
+  {
+    "revision": "44df1775f36ae7b0e17e",
+    "url": "/static/css/main.fd2f83a4.chunk.css"
+  },
+  {
+    "revision": "322373bd34e7f6ed8279857e6fe203de",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c8a594ce7a08e796edae4a9abc6898af.js"
+  "/precache-manifest.54ad518d1c83f9f89fa00cdf75e949e7.js"
 );
 
 workbox.clientsClaim();
